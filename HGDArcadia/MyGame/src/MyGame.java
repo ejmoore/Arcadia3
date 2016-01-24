b0196c48da28eeef122bf001e14b6d757546c902
PolyAtheist <tltaggar@c28-0117-18.ad.mtu.edu>
2016-01-24 15:33:17 -0500

    movement started git blows

--- HGDArcadia/MyGame/src/MyGame.java
+++ HGDArcadia/MyGame/src/MyGame.java
@@ -5,18 +5,19 @@ import java.util.*;
 import javax.imageio.ImageIO;
 
 import arcadia.*;
+import arcadia.Button;
 import basicGame.BasicGame;
 import intro.IntroGame;
 import shooter.Shooter;
 
 public class MyGame extends Game {
 
-	private final int width = 8;
-	private final int height = 100;
-	
 	Image banner;
-	Tile[][] tiles = new Tile[width][height];
-	
+	Tile[][] tiles = new Tile[20][100];
+	int startx = 10;
+	int starty = 0;
+	private final int width = 20;
+	private final int height = 100;
 	private final int tileSizeW = WIDTH / 8;
 	private final int tileSizeH = HEIGHT / 8;
 
@@ -35,7 +36,6 @@ public class MyGame extends Game {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
 		for (int j = 0; j < height; j++) {
 			for (int i = 0; i < width; i++) {
 				tiles[i][j] = new Tile(map.nextInt(), i, j, tileSizeW, tileSizeH);
@@ -48,15 +48,46 @@ public class MyGame extends Game {
 		g.setColor(Color.WHITE);
 		g.fillRect(0, 0, WIDTH, HEIGHT);
 
-		for (int i = 0; i < tiles.length; i++) {
-			for (int j = 0; j < tiles[i].length; j++) {
-				tiles[i][j].drawTile(g);
+		int k= 0;
+		for (int i = startx; i <= startx + 8; i++) {
+			int h = 0;
+			for (int j = starty; j <= starty + 8; j++) {
+				tiles[i][j].drawTile(g, k, h);
+				h++;
 			}
+			k++;
 		}
-
 		
 		g.setColor(Color.MAGENTA);
-		g.fillOval(WIDTH / 2 - tileSizeW / 2, HEIGHT / 2 - tileSizeH, tileSizeW, tileSizeH);
+	//drawImage(g, ship, WIDTH / 2, HEIGHT / 2, 0);
+		System.out.println(startx + " " + starty);
+		g.fillOval(WIDTH / 2 - tileSizeW , HEIGHT / 2 - tileSizeH, tileSizeW, tileSizeH);
+		
+		if (p1.pressed(Button.L)) {
+			if (tiles[startx + 2][starty + 3].tileType != 2)
+				if (startx != 0)
+					startx--;
+		} // Move left if player hit left
+		if (p1.pressed(Button.R)) {
+			if(startx<11)
+			if (tiles[startx + 4][starty + 3].tileType != 2)
+				startx++;
+		} // Move right if player hit right
+		if (p1.pressed(Button.D)) {
+			if (tiles[startx + 3][starty + 4].tileType != 2)
+				starty++;
+		} // Move down if player hit down
+		if (p1.pressed(Button.U)) {
+			if (tiles[startx + 3][starty + 2].tileType != 2)
+				if (starty != 0)
+					starty--;
+		} // Move up if player hit up
+		try {
+			Thread.sleep(30);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	public void createMap() {
