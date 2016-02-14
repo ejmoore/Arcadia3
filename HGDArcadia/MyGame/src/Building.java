5d44f9cb28e8fdb8b0e690e81689280d68f4b143
Eric Moore <ejmoore@c28-0117-16.ad.mtu.edu>
2016-02-14 13:00:23 -0500

    Changed building interface

--- HGDArcadia/MyGame/src/Building.java
+++ HGDArcadia/MyGame/src/Building.java
@@ -1,11 +1,13 @@
 import java.awt.Graphics2D;
 
+import arcadia.Input;
+
 public interface Building {
 	
 	static final int HEIGHT = 1024;
 	static final int WIDTH = 576;
 	
-	public void buildingControls();
+	public void buildingControls(Input p1, Input p2);
 	
 	public void drawBuilding(Graphics2D g);
 	
