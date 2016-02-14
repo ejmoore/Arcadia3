a35963cc8a1f19db9317794afed09aa8b12fbb2e
Robby Reid <rareid@c28-0117-15.ad.mtu.edu>
2016-02-14 12:26:19 -0500

    more bug fixes

--- HGDArcadia/MyGame/src/Tile.java
+++ HGDArcadia/MyGame/src/Tile.java
@@ -62,7 +62,7 @@ public class Tile {
 		}
 		g.fillRect((int) (x * tileSizeW), (int) (y * tileSizeH), tileSizeW,
 				tileSizeH+4);
-		if (tileType == 1) {
+		if (tileType == 1 || tileType == 98) {
 			g.drawImage(dirt, (int) (x * tileSizeW), (int) (y * tileSizeH), 113, 72, null);
 		}
 	}
